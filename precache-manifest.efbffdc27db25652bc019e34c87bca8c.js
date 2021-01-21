283c728622a78f83faa0bbdd98c0773854df40e1
Gauri Chavan Office <gaurichavanoffice@gauris-MacBook-Pro.local>
2021-01-21 17:32:13 -0500

    Updates

---
+++ precache-manifest.efbffdc27db25652bc019e34c87bca8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f528fa69e9c2511b883b7b3ad92149bf",
+    "url": "/SKSI/index.html"
+  },
+  {
+    "revision": "82ed1667d231e8dbf5dd",
+    "url": "/SKSI/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f7e72746f494b14beef0",
+    "url": "/SKSI/static/css/main.b59526e9.chunk.css"
+  },
+  {
+    "revision": "82ed1667d231e8dbf5dd",
+    "url": "/SKSI/static/js/2.cc561d13.chunk.js"
+  },
+  {
+    "revision": "573bca8b01fe535551b3952bcc2ddbc4",
+    "url": "/SKSI/static/js/2.cc561d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e72746f494b14beef0",
+    "url": "/SKSI/static/js/main.a686e971.chunk.js"
+  },
+  {
+    "revision": "5e8fd8f2304c0412b2b9",
+    "url": "/SKSI/static/js/runtime-main.51b6e995.js"
+  },
+  {
+    "revision": "31336773cff0eac270738c9e023cf78d",
+    "url": "/SKSI/static/media/background.31336773.jpg"
+  }
+]);
